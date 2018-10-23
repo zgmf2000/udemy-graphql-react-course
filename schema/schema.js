791a24d73843dea843d3fae891fdbf250d3e1df6
Bintoro Adi Guna <bintoro.guna@btpn.com>
2018-10-23 10:48:12 +0700

    Added RootQuery for basic user search

---
+++ schema/schema.js
@@ -0,0 +1,46 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} = graphql;
+const users = require('../fixtures/mock-user-data');
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: {
+      type: GraphQLString
+    },
+    firstName: {
+      type: GraphQLString
+    },
+    age: {
+      type: GraphQLInt
+    }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: {
+          type: GraphQLString
+        }
+      },
+      resolve(parentValue, args) {
+        const { id } = args;
+        return _.find(users, { id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
